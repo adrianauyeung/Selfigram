0905127919cfb307a76d4fdcfde1557b4796d8aa
adrian <adrian@eventbase.com>
2016-03-29 02:35:40 -0700

    W3D2 Selfie
    Complete - update feed to support selfies

---
+++ Selfigram/SelfieCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelfieCellTableViewCell.swift
+//  Selfigram
+//
+//  Created by adrian on 2016-03-29.
+//  Copyright © 2016 adrian. All rights reserved.
+//
+
+import UIKit
+
+class SelfieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
